d0824e3144c1aede27237a3cd27e824c86180233
JaykaStudios <jaykastudios@gmail.com>
2024-11-23 18:47:10 +0530

    thread off

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1732367188|8157255526';
+const CACHE_VERSION = '1732367804|8773758613';
 /** @type {string} */
 const CACHE_PREFIX = 'The Counterfeit -sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
